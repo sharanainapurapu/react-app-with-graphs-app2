072ac7eeadc507a208353e9f7640000f0fd5ec04
Sharan Ainapurapu <ainapurapu.sharan@siriuscom.com>
2020-04-08 15:17:47 +0530

    API

--- src/App.js
+++ src/App.js
@@ -1,159 +1,26 @@
 import React from 'react';
 import './App.css';
-import { ResponsiveBar } from '@nivo/bar';
-import { ResponsivePie } from '@nivo/pie';
 
-function App() {
-  let data_pie = [
-    {
-      "id": "elixir",
-      "label": "elixir",
-      "value": 462,
-      "color": "hsl(294, 70%, 50%)"
-    },
-    {
-      "id": "erlang",
-      "label": "erlang",
-      "value": 232,
-      "color": "hsl(11, 70%, 50%)"
-    },
-    {
-      "id": "python",
-      "label": "python",
-      "value": 347,
-      "color": "hsl(290, 70%, 50%)"
-    },
-    {
-      "id": "php",
-      "label": "php",
-      "value": 204,
-      "color": "hsl(43, 70%, 50%)"
-    },
-    {
-      "id": "make",
-      "label": "make",
-      "value": 311,
-      "color": "hsl(283, 70%, 50%)"
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {}
     }
-  ];
+    
+    componentDidMount(){
+        fetch("https://api.jsonbin.io/b/5e8d74eb980e481b8aa0c5ca")
+            .then((response) => response.json())
+            .then((response) => {this.setState(response);console.log(this.state)});   
+    }
 
-  return (
-     <div style={{height:'400px'}}>
-        <ResponsivePie
-                data={data_pie}
-                margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-                innerRadius={0.5}
-                padAngle={0.7}
-                cornerRadius={3}
-                colors={{ scheme: 'nivo' }}
-                borderWidth={1}
-                borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
-                radialLabelsSkipAngle={10}
-                radialLabelsTextXOffset={6}
-                radialLabelsTextColor="#333333"
-                radialLabelsLinkOffset={0}
-                radialLabelsLinkDiagonalLength={16}
-                radialLabelsLinkHorizontalLength={24}
-                radialLabelsLinkStrokeWidth={1}
-                radialLabelsLinkColor={{ from: 'color' }}
-                slicesLabelsSkipAngle={10}
-                slicesLabelsTextColor="#333333"
-                animate={true}
-                motionStiffness={90}
-                motionDamping={15}
-                defs={[
-                    {
-                        id: 'dots',
-                        type: 'patternDots',
-                        background: 'inherit',
-                        color: 'rgba(255, 255, 255, 0.3)',
-                        size: 4,
-                        padding: 1,
-                        stagger: true
-                    },
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'inherit',
-                        color: 'rgba(255, 255, 255, 0.3)',
-                        rotation: -45,
-                        lineWidth: 6,
-                        spacing: 10
-                    }
-                ]}
-                fill={[
-                    {
-                        match: {
-                            id: 'ruby'
-                        },
-                        id: 'dots'
-                    },
-                    {
-                        match: {
-                            id: 'c'
-                        },
-                        id: 'dots'
-                    },
-                    {
-                        match: {
-                            id: 'go'
-                        },
-                        id: 'dots'
-                    },
-                    {
-                        match: {
-                            id: 'python'
-                        },
-                        id: 'dots'
-                    },
-                    {
-                        match: {
-                            id: 'scala'
-                        },
-                        id: 'lines'
-                    },
-                    {
-                        match: {
-                            id: 'lisp'
-                        },
-                        id: 'lines'
-                    },
-                    {
-                        match: {
-                            id: 'elixir'
-                        },
-                        id: 'lines'
-                    },
-                    {
-                        match: {
-                            id: 'javascript'
-                        },
-                        id: 'lines'
-                    }
-                ]}
-                legends={[
-                    {
-                        anchor: 'bottom',
-                        direction: 'row',
-                        translateY: 56,
-                        itemWidth: 100,
-                        itemHeight: 18,
-                        itemTextColor: '#999',
-                        symbolSize: 18,
-                        symbolShape: 'circle',
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemTextColor: '#000'
-                                }
-                            }
-                        ]
-                    }
-                ]}
-        />      
-      </div> 
-  )
+    render() {
+        return (
+            <div className="wrapper">
+                <h2>{this.state.type}</h2>
+                <h4>{this.state.count}</h4>
+            </div> 
+        )
+    }
 }
 
 export default App;
